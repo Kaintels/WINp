9728f3fd84afc75c529a9c0bbd5f47dff40631ce
Han <seungwoohan0108@gmail.com>
2018-11-12 13:02:17 +0900

    feat : New file upload 20181112

---
+++ chap7_한승우_2/ex7_6/MoveResizeApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex7_6
+{
+    public partial class MoveResizeApp : Form
+    {
+        public MoveResizeApp()
+        {
+            InitializeComponent();
+        }
+
+        private void SetButtonText()
+        {
+            button1.Text = "Location = " + Location +"\n" + "Left/Right/Width = " + Left + ", " + Right +", " + Width +"\n" + "Top/Bottom/Height = " + Top +", " + Bottom +", " + Height;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetButtonText();
+;        }
+
+        private void button1_Move(object sender, EventArgs e)
+        {
+            SetButtonText();
+        }
+
+        private void button1_Resize(object sender, EventArgs e)
+        {
+            SetButtonText();
+        }
+    }
+}
